3eac20c9ffd146511f58c07d800249db7dd9f0bf
webmayak <web-mayak@mail.ru>
2017-11-28 00:37:27 +1000

    add admin menu injection on init() event

--- backend/Module.php
+++ backend/Module.php
@@ -2,7 +2,18 @@
 
 namespace pantera\news\backend;
 
+use yii\base\Event;
+use yii\bootstrap\Nav;
+
 class Module extends \yii\base\Module
 {
-
+	public function init() {
+		Event::on(Nav::className(), Nav::EVENT_INIT, function ($e) {
+		    $nav = $e->sender;
+		    if ($nav->id == 'admin-main-nav') {
+		    	$nav->items[] = ['label' => 'News', 'url' => ['/news']];
+		    }
+		});
+		parent::init();
+	}
 }
