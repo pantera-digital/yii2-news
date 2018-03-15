404b2aa820422738b207affb5ee4c10c02e714dd
singletonn <sopranowest@gmail.com>
2018-03-15 10:01:48 +1000

    формирование title у страниц новостей http://new.kid.travel/news/?page=14&per-page=10 title= Новости о детском отдыхе и лагерях - страница [номер страницы]

--- frontend/views/default/index.php
+++ frontend/views/default/index.php
@@ -5,10 +5,7 @@ use yii\data\ActiveDataProvider;
 use yii\widgets\ListView;
 
 /* @var $dataProvider ActiveDataProvider */
-$title = 'Новости о детском отдыхе и лагерях';
-if(Yii::$app->request->get('page')){
-    $title .= ' - страница ' . Yii::$app->request->get('page');
-}
+$title = 'Новости о детском отдыхе и лагерях - страница ' . Yii::$app->request->get('page', 1);
 $this->title = $title;
 $this->params['breadcrumbs'][] = 'Новости';
 ?>
