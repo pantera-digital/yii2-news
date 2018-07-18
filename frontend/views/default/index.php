8f547200dcc03720740596453532395f4a4cc62a
singletonn <sopranowest@gmail.com>
2018-07-18 15:56:41 +1000

    для страниц пагинации реклканоникал на 1 страницу паагинации - https://new.kid.travel/news/?page=8

--- frontend/views/default/index.php
+++ frontend/views/default/index.php
@@ -2,12 +2,21 @@
 
 use pantera\news\frontend\widgets\tagList\NewsTagList;
 use yii\data\ActiveDataProvider;
+use yii\helpers\Url;
+use yii\web\View;
 use yii\widgets\ListView;
 
 /* @var $dataProvider ActiveDataProvider */
+/* @var $this View */
 $title = 'Новости о детском отдыхе и лагерях - страница ' . Yii::$app->request->get('page', 1);
 $this->title = $title;
 $this->params['breadcrumbs'][] = 'Новости';
+if (Yii::$app->request->get('page')) {
+    $this->registerLinkTag([
+        'rel' => 'canonical',
+        'href' => Url::to(['index']),
+    ]);
+}
 ?>
 
 <h1>Все новости</h1>
